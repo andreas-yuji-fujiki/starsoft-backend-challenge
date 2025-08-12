0c20fc4b3f815425ec3db78c6df4cf0bf556f711
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-08-11 22:24:22 -0300

    feat: implement orders controller with CRUD operations and search functionality

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, Query } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { Order } from './order.entity';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('orders')
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @Post()
+  create(@Body() orderData: Partial<Order>): Promise<Order> {
+    return this.ordersService.create(orderData);
+  }
+
+  @Get()
+  findAll(): Promise<Order[]> {
+    return this.ordersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<Order | null> {
+    return this.ordersService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateData: Partial<Order>): Promise<Order | null> {
+    return this.ordersService.update(id, updateData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<void> {
+    return this.ordersService.remove(id);
+  }
+
+  @Get('search')
+  search(@Query() query: any): Promise<any> {
+    return this.ordersService.search(query);
+  }
+}
