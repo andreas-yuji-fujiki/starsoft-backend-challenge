749a876e3e57fd2817233259c480b66e5a0f20b9
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-08-11 21:21:23 -0300

    feat: create typeorm module config for root on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Order } from './orders/order.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST || 'postgres',
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [
+        __dirname + '/**/*.entity{.ts,.js}',
+        Order
+      ],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Order]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
